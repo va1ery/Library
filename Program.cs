e6f68f2639d1e9077f3d8f3b405501bbb3c97cbc
va1ery <valery.gourov@gmail.com>
2020-10-02 09:44:38 +0300

    modified Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/* using System;
 
 namespace Library
 {
@@ -7,6 +7,50 @@ namespace Library
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+}
+*/
+using System;
+using System.Linq;
+
+namespace Library.Models
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new LibraryContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new Position");
+                db.Add(new Position { PositionName = "Manager" });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a Position");
+                var position = db.Positions
+                    .OrderBy(b => b.ID)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the position and adding a employee");
+                position.PositionName = "Manager";
+        /*        position.Employee.Add(
+                    new Employee
+                    {
+                        LastName = "Sidorov",
+                        FirstName = "Peter",
+        //                BirthDate = 10-10-2010,
+                        //PositionID = 
+                    });
+                db.SaveChanges();
+
+                /* Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();*/
+            }
         }
     }
 }
